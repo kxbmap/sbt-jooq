1e080807bdeafe2e356e81fde7a0636a7a8457d8
kxbmap <kxbmap@gmail.com>
2017-10-23 21:45:43 +0900

    Change way to detect generated sources

--- src/main/scala/com/github/kxbmap/sbt/jooq/CodegenUtil.scala
+++ src/main/scala/com/github/kxbmap/sbt/jooq/CodegenUtil.scala
@@ -1,21 +1,11 @@
 package com.github.kxbmap.sbt.jooq
 
-import sbt._
 import scala.xml.transform.RewriteRule
 
 object CodegenUtil {
 
   def isJigsawEnabled(javaVersion: String): Boolean =
     javaVersion.takeWhile(_.isDigit).toInt >= 9
-
-
-  val sourceFilter: FileFilter = "*.java" || "*.scala"
-
-  def listSourcesIn(directory: File): Seq[File] =
-    (directory ** sourceFilter).get
-
-  def listSourcesIn(directories: Seq[File]): Seq[File] =
-    (directories ** sourceFilter).get
 
 
   def rewriteRule(name0: String)(f: PartialFunction[xml.Node, Seq[xml.Node]]): RewriteRule = new RewriteRule {
