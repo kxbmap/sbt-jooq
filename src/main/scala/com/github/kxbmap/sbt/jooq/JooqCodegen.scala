62e73ef298037d2d6ad3183aee89c28a7b93ad9f
kxbmap <kxbmap@gmail.com>
2018-06-28 19:09:39 +0900

    Set DefaultJooqVersion to 3.11.2

--- src/main/scala/com/github/kxbmap/sbt/jooq/JooqCodegen.scala
+++ src/main/scala/com/github/kxbmap/sbt/jooq/JooqCodegen.scala
@@ -14,7 +14,7 @@ import scala.xml.{Node, Text, XML}
 
 object JooqCodegen extends AutoPlugin {
 
-  val DefaultJooqVersion = "3.11.1"
+  val DefaultJooqVersion = "3.11.2"
 
   override def requires: Plugins = Slf4jSimplePlugin
 
