b976f4ec55b9ca25ae054511f63e7b7764acf984
kxbmap <kxbmap@gmail.com>
2021-09-15 23:23:30 +0900

    Disable scripted tests that not corresponding to earlier JDK version on CI.

--- project/ScriptedSettings.scala
+++ project/ScriptedSettings.scala
@@ -1,4 +1,5 @@
 import Versions._
+import java.nio.file.{Files, Path}
 import sbt.Keys._
 import sbt.ScriptedPlugin.autoImport._
 import sbt._
@@ -9,6 +10,12 @@ object ScriptedSettings extends AutoPlugin {
   override def requires: Plugins = SbtPlugin
 
   override def trigger: PluginTrigger = allRequirements
+
+  object autoImport {
+    val disableScriptedTestsForEarlierJava = taskKey[Unit]("")
+  }
+
+  import autoImport._
 
   override def projectSettings: Seq[Setting[_]] = Seq(
     scriptedSbt := sbtVersion.value,
@@ -22,7 +29,8 @@ object ScriptedSettings extends AutoPlugin {
       s"-Dflywaysbt.version=$flywaySbtVersion"
     ) ++ jooqVersions.map { v =>
       s"-Djooq.${minorVersion(v).replace('.', '_')}.version=$v"
-    }
+    },
+    disableScriptedTestsForEarlierJava := disableScriptedTestsTask.value
   )
 
   private def javaVersion = sys.props("java.version").takeWhile(_.isDigit).toInt
@@ -32,5 +40,26 @@ object ScriptedSettings extends AutoPlugin {
       jooqVersionForJava8
     else
       jooqVersion
+
+  private def disableScriptedTestsTask = Def.task[Unit] {
+    if (javaVersion < 11) {
+      def isTarget(name: String): Boolean = {
+        val m = """jooq-(\d+\.\d+)""".r.pattern.matcher(name)
+        m.matches() && VersionNumber(m.group(1)).matchesSemVer(SemanticSelector(">=3.15"))
+      }
+      def disableTest(path: Path): Unit = {
+        val disable = path / "disable"
+        if (Files.notExists(disable)) Files.createFile(disable)
+      }
+      val dir = sbtTestDirectory.value.toPath
+      if (Files.isDirectory(dir))
+        Files.list(dir)
+          .filter(Files.isDirectory(_))
+          .flatMap(Files.list(_))
+          .filter(Files.isDirectory(_))
+          .filter(path => isTarget(path.getFileName.toString))
+          .forEach(disableTest)
+    }
+  }
 
 }
