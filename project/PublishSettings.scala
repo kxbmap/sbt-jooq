06565136fd5e8cb364d6777e4b4d40f4dc8827e8
kxbmap <kxbmap@gmail.com>
2021-09-30 19:46:12 +0900

    Set versionScheme setting

--- project/PublishSettings.scala
+++ project/PublishSettings.scala
@@ -1,5 +1,5 @@
-import sbt.Keys._
 import sbt._
+import sbt.Keys._
 import xerial.sbt.Sonatype
 import xerial.sbt.Sonatype.GitHubHosting
 import xerial.sbt.Sonatype.SonatypeKeys._
@@ -10,15 +10,18 @@ object PublishSettings extends AutoPlugin {
 
   override def trigger: PluginTrigger = allRequirements
 
-  override def projectSettings: Seq[Setting[_]] = Seq(
+  override def buildSettings: Seq[Setting[_]] = Seq(
     description := "jOOQ plugin for sbt 1.0+",
     organization := "com.github.kxbmap",
-
-    publishMavenStyle := true,
-    publishTo := sonatypePublishToBundle.value,
+    versionScheme := Some("early-semver"),
     licenses := Seq(
       "Apache License, Version 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0.html")
     ),
+  )
+
+  override def projectSettings: Seq[Setting[_]] = Seq(
+    publishMavenStyle := true,
+    publishTo := sonatypePublishToBundle.value,
     sonatypeProjectHosting := Some(GitHubHosting("kxbmap", "sbt-jooq", "Tsukasa Kitachi", "kxbmap@gmail.com")),
     pomIncludeRepository := { _ => false }
   )
