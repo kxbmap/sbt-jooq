bcd78bb4e4138e1195860b0910395b440effeb21
kxbmap <kxbmap@gmail.com>
2021-10-11 21:53:46 +0900

    Remove object JooqCodegenInternalKeys

--- codegen/src/main/scala/sbtjooq/codegen/JooqCodegenKeys.scala
+++ codegen/src/main/scala/sbtjooq/codegen/JooqCodegenKeys.scala
@@ -16,25 +16,18 @@ trait JooqCodegenKeys {
   val jooqCodegenConfig = settingKey[CodegenConfig]("jOOQ-codegen configuration")
   val jooqCodegenVariables = settingKey[Map[String, Any]]("Variables to replace configuration placeholders")
 
-  // For reference
+  // Customizable
   val jooqSource = settingKey[File]("Default generated jOOQ source directory")
-  val jooqCodegenConfigFiles = taskKey[Seq[File]]("Actual jOOQ-codegen configuration files")
-  val jooqCodegenGeneratedSources = taskKey[Seq[File]]("jOOQ-codegen generated sources")
-
-}
-
-object JooqCodegenKeys extends JooqCodegenKeys
-
-
-trait JooqCodegenInternalKeys {
-
-  // For internal use
   val jooqCodegenConfigTransformer = settingKey[Node => Node]("jOOQ-codegen configuration transform function")
   val jooqCodegenVariableExpander = settingKey[PartialFunction[Any, NodeSeq]]("Partial function to expand variables")
+
+  // Internal use
   val jooqCodegenTransformedConfigs = taskKey[Seq[Node]]("Transformed jOOQ-codegen configurations")
+  val jooqCodegenConfigFiles = taskKey[Seq[File]]("Actual jOOQ-codegen configuration files")
+  val jooqCodegenGeneratedSources = taskKey[Seq[File]]("jOOQ-codegen generated sources")
   val jooqCodegenGeneratorTargets = taskKey[Seq[(File, File)]]("jOOQ-codegen generator target directory")
   val jooqCodegenGeneratedSourcesFinders = taskKey[Seq[(File, PathFinder)]]("PathFinders for generated sources")
 
 }
 
-object JooqCodegenInternalKeys extends JooqCodegenInternalKeys
+object JooqCodegenKeys extends JooqCodegenKeys
