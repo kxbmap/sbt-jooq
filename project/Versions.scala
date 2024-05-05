cd500418547db4694e2555c4da6adc0de5a66385
kxbmap <kxbmap@gmail.com>
2024-05-05 17:41:32 +0900

    Update versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,16 +2,16 @@ import sbt.librarymanagement.VersionNumber
 
 object Versions {
 
-  final val scala212 = "2.12.18"
-  final val scala213 = "2.13.12"
-  final val scala3 = "3.3.1"
+  final val scala212 = "2.12.19"
+  final val scala213 = "2.13.14"
+  final val scala3 = "3.3.3"
 
-  final val jooqVersion = "3.18.7"
+  final val jooqVersion = "3.18.15"
 
   val jooqVersions: Seq[String] = Seq(
     jooqVersion,
-    "3.17.16",
-    "3.16.22",
+    "3.17.24",
+    "3.16.23",
     "3.15.12",
     "3.14.16",
     "3.13.6",
@@ -20,15 +20,15 @@ object Versions {
     "3.10.8"
   )
 
-  final val scalaXMLVersion = "2.2.0"
+  final val scalaXMLVersion = "2.3.0"
 
-  final val logbackVersion = "1.3.11"
+  final val logbackVersion = "1.3.14"
 
   final val h2Version = "2.1.214"
 
   final val h2V1Version = "1.4.200"
 
-  final val wartRemoverVersion = "3.1.5"
+  final val wartRemoverVersion = "3.1.6"
 
   final val javaxActivationVersion = "1.1.1"
 
@@ -42,7 +42,7 @@ object Versions {
 
   final val flywaySbtVersion = "7.4.0"
 
-  final val scalaTestVersion = "3.2.17"
+  final val scalaTestVersion = "3.2.18"
 
   def minorVersion(version: String): String =
     version match {
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -5,11 +5,11 @@ scalacOptions ++= Seq(
   "-Xlint:-unused,_"
 )
 
-addSbtPlugin("com.github.sbt" % "sbt-release" % "1.1.0")
-addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "3.9.21")
+addSbtPlugin("com.github.sbt" % "sbt-release" % "1.4.0")
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "3.10.0")
 addSbtPlugin("com.github.sbt" % "sbt-pgp" % "2.2.1")
-addSbtPlugin("org.scalameta" % "sbt-mdoc" % "2.4.0")
-addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.11.0")
+addSbtPlugin("org.scalameta" % "sbt-mdoc" % "2.5.2")
+addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.12.0")
 addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.5.2")
 addSbtPlugin("de.heikoseeberger" % "sbt-header" % "5.10.0")
-addSbtPlugin("ch.epfl.scala" % "sbt-version-policy" % "2.1.3")
+addSbtPlugin("ch.epfl.scala" % "sbt-version-policy" % "3.2.1")
