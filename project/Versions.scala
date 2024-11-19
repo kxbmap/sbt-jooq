13156b0853155e1e027f6f04541a632ae59a453e
kxbmap <kxbmap@gmail.com>
2024-11-19 16:07:36 +0900

    Update versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,16 +2,16 @@ import sbt.librarymanagement.VersionNumber
 
 object Versions {
 
-  final val scala212 = "2.12.19"
-  final val scala213 = "2.13.14"
-  final val scala3 = "3.3.3"
+  final val scala212 = "2.12.20"
+  final val scala213 = "2.13.15"
+  final val scala3 = "3.5.2"
 
-  final val jooqVersion = "3.19.8"
+  final val jooqVersion = "3.19.15"
 
   val jooqVersions: Seq[String] = Seq(
     jooqVersion,
-    "3.18.15",
-    "3.17.24",
+    "3.18.22",
+    "3.17.31",
     "3.16.23",
     "3.15.12",
     "3.14.16",
@@ -25,11 +25,11 @@ object Versions {
 
   final val logbackVersion = "1.3.14"
 
-  final val h2Version = "2.1.214"
+  final val h2Version = "2.3.232"
 
   final val h2V1Version = "1.4.200"
 
-  final val wartRemoverVersion = "3.1.6"
+  final val wartRemoverVersion = "3.2.4"
 
   final val javaxActivationVersion = "1.1.1"
 
@@ -43,7 +43,7 @@ object Versions {
 
   final val flywaySbtVersion = "7.4.0"
 
-  final val scalaTestVersion = "3.2.18"
+  final val scalaTestVersion = "3.2.19"
 
   def minorVersion(version: String): String =
     version match {
